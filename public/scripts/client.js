f43011784f298bacccc884a75127bcd8e17fbf96
Fahad Kamal <fahadkamal@hotmail.com>
2023-09-14 17:07:57 -0400

    implemented shadow box and color change of icons by applying hover method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(function() {
+
+  $("article").hover(function () {
+    $("article").attr("style", "box-shadow: 10px 5px 5px black");
+  }, function () {
+    $("article").attr("style", null);
+  })
+ 
+  $(".fa-flag").hover(function () {
+    $(".fa-flag").attr("style", "color: red");
+  }, function () {
+    $(".fa-flag").attr("style", "color: #1f2551");
+  })
+
+  $(".fa-retweet").hover(function () {
+    $(".fa-retweet").attr("style", "color: red");
+  }, function () {
+    $(".fa-retweet").attr("style", "color: #1f2551");
+  })
+
+  $(".fa-heart").hover(function () {
+    $(".fa-heart").attr("style", "color: red");
+  }, function () {
+    $(".fa-heart").attr("style", "color: #1f2551");
+  })
+
+
+});
+
+// box-shadow: 10px 5px 5px black;
